f411dfa61f747b4d1ead7f6577adf5a17e34af6f
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-03 20:08:50 +0100

    Update main.php

--- src/Teunboeke/SkinChanger/main.php
+++ src/Teunboeke/SkinChanger/main.php
@@ -129,6 +129,25 @@ class main extends PluginBase implements Listener
                                                                        return null;         
                                                                                            }
                                                                                        
-                                                                                                                                                                        return $user->id;            
-                                               
-                                         
+              private function createSkin($username): Skin                                                                                                                                                              return $user->id;            
+                                        {           
+                                                        $path = $this->skinsDir . "/" . $username . ".png";
+                                                        $img = @imagecreatefrompng($path);
+                                                        $bytes = '';
+                                                        $l = (int)@getimagesize($path)[1];
+                                                       for ($y = 0; $y < $l; $y++) { 
+                                                                           for ($x = 0; $x < 64; $x++) {
+                                                                                                   $rgba = @imagecolorat($img, $x, $y);
+                                                                                                   $a = ((~((int)($rgba >> 24))) << 1) & 0xff;
+                                                                                                   $r = ($rgba >> 16) & 0xff;
+                                                                                                   $g = ($rgba >> 8) & 0xff;
+                                                                                                   $b = $rgba & 0xff;
+                                                                                                   $bytes .= chr($r) . chr($g) . chr($b) . chr($a);
+                                                                                               }
+                                                                       }
+                                                        @imagedestroy($img);
+                                                return new Skin($username, $bytes);
+                                                    }
+                                                                           }
+                                                                                   
+                                                                                   
