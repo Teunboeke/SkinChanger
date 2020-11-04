f24442059ae2e3efca785ba247375bb1027049cc
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-04 08:37:27 +0100

    Update main.php

--- src/Teunboeke/SkinChanger/main.php
+++ src/Teunboeke/SkinChanger/main.php
@@ -126,7 +126,7 @@ public function onCommand(CommandSender $sender, Command $command, string $label
                                    if ($result) {
                                            return json_decode($result);
                                                    }
-                                                                       return null;         
+                                                       return null;         
                                                                                            }
                                                                                        
               private function createSkin($username): Skin                                                                                                                                                              return $user->id;            
@@ -136,14 +136,14 @@ public function onCommand(CommandSender $sender, Command $command, string $label
                                                         $bytes = '';
                                                         $l = (int)@getimagesize($path)[1];
                                                        for ($y = 0; $y < $l; $y++) { 
-                                                                           for ($x = 0; $x < 64; $x++) {
-                                                                                                   $rgba = @imagecolorat($img, $x, $y);
-                                                                                                   $a = ((~((int)($rgba >> 24))) << 1) & 0xff;
-                                                                                                   $r = ($rgba >> 16) & 0xff;
-                                                                                                   $g = ($rgba >> 8) & 0xff;
-                                                                                                   $b = $rgba & 0xff;
-                                                                                                   $bytes .= chr($r) . chr($g) . chr($b) . chr($a);
-                                                                                               }
+                                                       for ($x = 0; $x < 64; $x++) {
+                                                      $rgba = @imagecolorat($img, $x, $y);
+                                                      $a = ((~((int)($rgba >> 24))) << 1) & 0xff;
+                                                      $r = ($rgba >> 16) & 0xff;
+                                                      $g = ($rgba >> 8) & 0xff;
+                                                      $b = $rgba & 0xff;
+                                                      $bytes .= chr($r) . chr($g) . chr($b) . chr($a);
+                                                                                  }
                                                                        }
                                                         @imagedestroy($img);
                                                 return new Skin($username, $bytes);
